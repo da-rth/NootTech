eeeecb1668847df85a981da49823f06283dbf263
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2019-02-14 17:42:09 +0000

    added bootstrap-vue

--- NootTech/frontend/src/main.js
+++ NootTech/frontend/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+
 
 Vue.prototype.$api_url = 'http://localhost:8000/api';
 Vue.prototype.$subdomain_url = ".noot.tech";
 Vue.prototype.$subdomain_enabled = false;
+
+// load BootstrapVue
+Vue.use(BootstrapVue);
 
 new Vue({
   el: '#app',
