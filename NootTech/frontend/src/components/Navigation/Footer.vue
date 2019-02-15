264d55e007c3d5b83d5ff8415d0b73914095cdb8
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2019-02-15 05:56:25 +0000

    added footer stub

---
+++ NootTech/frontend/src/components/Navigation/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- Footer -->
+<footer class="page-footer font-small gray">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href="#">Noot.Tech</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</template>
+
+<script>
+    export default {
+        name: "NtFooter"
+    }
+</script>
--- NootTech/frontend/src/components/Navigation/MainPane.vue
+++ NootTech/frontend/src/components/Navigation/MainPane.vue
@@ -1,19 +1,22 @@
 <template>
-    <main>
-        <b-container fluid class="bv-example-row bg-dark">
-            <b-form-row>
+    <b-container fluid class="bv-example-row bg-dark">
+        <b-form-row>
+            <main>
                 <slot></slot>
-            </b-form-row>
-        </b-container>
-    </main>
+            </main>
+        </b-form-row>
+        <nt-footer></nt-footer>
+    </b-container>
 </template>
 <script>
+    import NtFooter from "./Footer.vue";
+
     export default {
         name: "NtMainPane",
         props: ["title"],
         created: function() {
             document.title = this.title
-
-        }
+        },
+        components: {NtFooter}
    }
 </script>
