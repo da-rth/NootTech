47d4397de9e60891225d554cc944334475a0426b
denBot <den.xyz@pm.me>
2019-02-18 02:05:52 +0000

    pagination, show upload key, select files done - file grid needs worked on

--- NootTech/frontend/src/components/FilePanel/Panel.vue
+++ NootTech/frontend/src/components/FilePanel/Panel.vue
@@ -1,8 +1,95 @@
 <template>
-  <div>
-        <h4 v-for="file in $parent.files" style="color: white">
-          {{ file.generated_filename}} - {{ file.date }}
-        </h4>
+  <div class="file-panel">
+    <b-navbar toggleable="lg" type="dark" class="file-menubar">
+
+      <b-navbar-toggle target="nav_file_collapse" />
+
+      <b-collapse is-nav id="nav_file_collapse">
+
+        <b-button-group>
+          <b-button class="filebar-btn">Small</b-button>
+          <b-button class="filebar-btn">Med</b-button>
+          <b-button class="filebar-btn">Large</b-button>
+        </b-button-group>
+
+        <b-input-group prepend="K" class="filebar-uploadkey" v-if="showUploadKey">
+          <b-form-input class="key-field" v-bind:value="$parent.settings.upload_key" readonly/>
+          <b-input-group-append>
+            <b-button>Copy</b-button>
+          </b-input-group-append>
+        </b-input-group>
+
+        <b-form-checkbox class="select-files-switch" switch v-model="showUploadKey" name="check-button">
+          {{ showUploadKey ? "Hide Key" : "Show Key" }}
+        </b-form-checkbox>
+
+        <b-form-checkbox class="select-files-switch" switch v-model="selectFiles" name="check-button">
+          Select Files...
+        </b-form-checkbox>
+
+        <b-dropdown text="File List" v-if="selectedFiles.length > 0 && selectFiles">
+          <b-dropdown-item v-for="file in getCheckedFiles()">
+            {{ file.generated_filename }} | {{ file.original_filename }}
+          </b-dropdown-item>
+        </b-dropdown>
+
+
+        <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2 filebar-search" type="text" placeholder="Search"/>
+        </b-nav-form>
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content"><em>Filesize</em></template>
+            <b-dropdown-item href="#">Image</b-dropdown-item>
+            <b-dropdown-item href="#">Audio</b-dropdown-item>
+            <b-dropdown-item href="#">Video</b-dropdown-item>
+            <b-dropdown-item href="#">Text</b-dropdown-item>
+            <b-dropdown-item href="#">Other</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content"><em>Type</em></template>
+            <b-dropdown-item href="#">Image</b-dropdown-item>
+            <b-dropdown-item href="#">Audio</b-dropdown-item>
+            <b-dropdown-item href="#">Video</b-dropdown-item>
+            <b-dropdown-item href="#">Text</b-dropdown-item>
+            <b-dropdown-item href="#">Other</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <paginate name="searched_files" :list="$parent.searched_files" :per="paginate_by" tag="div" class="row file-row">
+
+      <b-form-checkbox-group v-model="selectedFiles">
+        <b-row class="file-grid justify-content-center">
+          <b-col v-bind:id="file.id" cols="1" lg="1" v-for="file in paginated('searched_files')" class="file-block" v-bind:style="{ 'background-image': 'url(' + file.file_thumbnail + ')' }">
+            <b-form-checkbox v-if="selectFiles" v-bind:value="file.id" @change="changeBorder"></b-form-checkbox>
+          </b-col>
+        </b-row>
+      </b-form-checkbox-group>
+
+    </paginate>
+
+    <paginate-links class="file-pagination" for="searched_files"
+                    :hide-single-page="true"
+                    :classes="{'ul': 'pagination', 'li': 'page-item', 'a' : 'page-link'}"
+                    :show-step-links="true"
+                    :step-links="{next: 'Next', prev: 'Previous'}">
+    </paginate-links>
+
+        <div class="text-center">
+      Selected files: {{ selectedFiles }}
+      <br/>
+      <b-button class="filebar-btn" variant="danger" v-if="selectFiles">Delete files</b-button>
+      <b-button class="filebar-btn" variant="primary" v-if="selectFiles">Make files private</b-button>
+    </div>
+
   </div>
 </template>
 
@@ -10,238 +97,111 @@
 
   export default {
     name: "FilePanel",
+
+    data() {
+      return {
+        selectedFiles: [],
+        selectFiles: false,
+        showUploadKey: false,
+        checkFilesDeletion: false,
+        checkFilesPrivate: false,
+        paginate: ['searched_files'],
+        page: {},
+        paginate_by: 60,
+      }
+    },
+    methods: {
+      changeBorder(checked) {
+        // User can select files to either delete or set private, but not both at the same time.
+        console.log(JSON.stringify(checked))
+        /**
+         if (this.checkFilesDeletion) {
+          console.log("change border colour of file to red")
+        } else if (this.checkFilesPrivate) {
+          console.log("change border colour of file to blue")
+        }
+         */
+      },
+      getCheckedFiles() {
+        let files = this.$parent.files.filter(f => this.selectedFiles.includes(f.id));
+        console.log(files)
+        return files
+      },
+    }
   }
 </script>
 
-<style>
-    .file-views {
-        opacity: 0;
-        position: absolute;
-        left: 5px;
-        top: 5px;
-        background-color: #3d3d3d;
-        padding: 1px 6px;
-        border-radius: 2px;
-        font-size: 11px;
-        user-select: none;
-        transition: opacity 0.2s ease-in-out;
-        border: 1px solid #FFFFFF11;
-    }
-    .file-href:hover .file-views {
-        opacity: 1;
-    }
-    .file-label {
-        width: 120px;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-    .delete-alert {
-        text-align: center;
-        margin-top: 5px;
-        margin-bottom: -5px;
-        background-color: #242424;
-    }
-    .deletion-border {
-        border: 1px solid red !important;
-    }
-    .thumbnail-play {
-        color: #d0d0d0;
-        position: absolute;
-        font-size: 2em;
-        width: 100%;
-        margin-top: 18.5%;
-        pointer-events: none;
-        transition: color 0.2s ease-in-out;
-        opacity: 0.95;
-        text-shadow: 0 0 2px #000000;
-    }
-    .file-block {
-        position: relative;
-    }
-    .file-block:hover .thumbnail-play {
-        color: #FFF;
-        opacity: 0.9;
-    }
-    .file-label {
-        transition: color 0.2s ease-in-out;
-        font-family: 'Source Sans Pro', sans-serif;
-    }
-    .file-block:hover .file-label {
-        color: #00cccc;
-        cursor: pointer;
-    }
-    .col-fb-lg .file-awesome {
-        font-size: 3.9em;
-        padding-top: 7%;
-    }
-    .col-fb-md .file-awesome {
-        font-size: 3em;
-        padding-top: 7%;
-    }
-    .col-fb-sm .file-awesome {
-        font-size: 2.5em;
-        padding-top: 7%;
-    }
-    .file-row {
-        padding-top: 20px;
-    }
-    /* Image Thumbnail */
-    .img-thumbnail {
-        background-color: rgba(0,0,0,0.4) !important;
-        cursor: pointer;
-        transition: border 0.2s ease-in-out;
-    }
-    .normal-border {
-        border: 1px solid black !important;
-    }
-    .normal-border:hover {
-        border: 1px solid rgba(144, 144, 144, 0.6) !important;
-    }
-    /* Scrollbar */
-    .container-fluid.file-container::-webkit-scrollbar {
-        width: 8px;
-    }
-    .container-fluid.file-container::-webkit-scrollbar-track {
-        box-shadow: none;
-    }
-    .container-fluid.file-container::-webkit-scrollbar-thumb {
-        background: transparent;
-        border-radius: 3px;
-    }
-    .container-fluid.file-container:hover::-webkit-scrollbar-thumb {
-        background: #3d3d3d;
-        border-radius: 3px;
-    }
-    .col-fb-lg,
-    .col-fb-md,
-    .col-fb-sm {
-        margin: 5px;
-        transition: all 0.4s ease-in-out;
-    }
-    .col-fb-lg .file-label {
-        font-size: 15px;
-    }
-    .col-fb-md .file-label {
-        font-size: 14px;
-    }
-    .col-fb-sm .file-label {
-        font-size: 12px;
-    }
-    @media (max-width: 576px) {
-        .col-fb-lg {
-            width: 93%;
-        }
-        img.img-fluid.img-thumbnail.file-thumb {
-            width: 100%;
-        }
-        .col-fb-md {
-            width: 45%;
-        }
-        .col-fb-sm {
-            width: 29.5%;
-        }
-        .row.file-row {
-            margin-left: 1%;
-        }
-    }
-    @media (min-width: 576px) {
-        .col-fb-lg {
-            width: 46%;
-        }
-        img.img-fluid.img-thumbnail.file-thumb {
-            width: 100%;
-        }
-        .col-fb-md {
-            width: 30%;
-        }
-        .col-fb-sm {
-            width: 22%;
-        }
-        .row.file-row {
-            margin-left: 1%;
-        }
-    }
-    /*Medium devices (tablets, 768px and up)*/
-    @media (min-width: 768px) {
-        .col-fb-lg {
-            width: 23%
-        }
-        .col-fb-md {
-            width: 18%
-        }
-        .col-fb-sm {
-            width: 15%
-        }
-        .row.file-row {
-            margin-left: 10px;
-        }
-    }
-    /*Large devices (desktops, 992px and up)*/
-    @media (min-width: 992px) {
-        .col-fb-lg {
-            width: 18.3%
-        }
-        .col-fb-md {
-            width: 15%
-        }
-        .col-fb-sm {
-            width: 11%
-        }
-        .row.file-row {
-            margin-left: 20px;
-        }
-    }
-    /*Extra large devices (large desktops, 1200px and up)*/
-    @media (min-width: 1200px) {
-        .col-fb-lg {
-            width: 15.5%;
-        }
-        .col-fb-md {
-            width: 10.1%;
-        }
-        .col-fb-sm {
-            width: 8.1%;
-        }
-        .row.file-row {
-            margin-left: 2%;
-        }
-    }
-    /*Extra extra large devices (extra large desktops, 1920px and up)*/
-    @media (min-width: 1920px) {
-        .col-fb-lg {
-            width: 13.5%;
-        }
-        .col-fb-md {
-            width: 10.4%;
-        }
-        .col-fb-sm {
-            width: 8.45%;
-        }
-        .row.file-row {
-            margin-left: 1.5%;
-        }
-        .file-label {
-            font-size: 18px !important;
-        }
-    }
-    .file-thumb {
-        object-fit: contain;
-        padding: 1px;
-        border-radius: 2px;
-        border: none;
-        transition: background-color 0.1s ease-in-out;
-        height: 79.8%;
-    }
-    .file-delete-checkbox {
-        display: none;
-        position: absolute;
-        right: 5px;
-        top: 5px;
-        opacity: 0.7;
-        transition: opacity 0.2s ease-in-out;
-    }
-    .file-block:hover .file-delete-checkbox {
-        opacity: 1;
-    }
+<style scoped>
+  * {
+    color: #b8b8b8;
+  }
+  .file-panel {
+    width: 92vw;
+    height: 78vh;
+    margin: 30px auto;
+  }
+  .file-menubar, .file-grid {
+    margin-bottom: 20px;
+    border: 1px solid #121212;
+    background-color: #1e1e1e !important;
+  }
+  .file-grid {
+    color: #9c9c9c;
+    padding: 10px;
+    max-height: 72vh;
+    margin-bottom: 10px;
+    overflow-y: scroll;
+  }
+
+  .file-block {
+    margin: 10px;
+    border-radius: 4px;
+    background-size: cover;
+    border: 1px solid #191919;
+    width: auto;
+    height: 100px;
+  }
+
+  .file-block:hover {
+    border-color: #fff;
+  }
+
+  .file-checkbox-group {
+    width: 100%;
+    height: 100%;
+  }
+
+  .file-pagination {
+    justify-content: center;
+  }
+  .select-files-switch {
+    margin: 0px 7px;
+  }
+  .filebar-btn {
+    margin: 0px 5px;
+    height: 34px;
+    padding: 5px 10px;
+  }
+  .filebar-uploadkey {
+    width: 300px;
+  }
+  {
+    font-size: 12px;
+  }
+  .filebar-uploadkey .btn,
+  .filebar-uploadkey .input-group-text,
+  .filebar-uploadkey .key-field {
+    font-size: 12px;
+  }
+
+  .filebar-uploadkey .input-group-text,
+  .filebar-uploadkey .key-field {
+    color: #242424;
+  }
+
+  .filebar-search {
+    width: 15vw;
+    font-size: 12px;
+  }
+
 </style>
--- NootTech/frontend/src/components/Navigation/Navbar.vue
+++ NootTech/frontend/src/components/Navigation/Navbar.vue
@@ -4,9 +4,10 @@
             <b-navbar-toggle target="nav_collapse" />
             <b-collapse is-nav id="nav_collapse">
 
-                <b-dropdown v-bind:text="$store.state.user.username" v-if="$store.state.user.authenticated">
-                  <b-dropdown-item href="#">An item</b-dropdown-item>
-                  <b-dropdown-item href="#">Another item</b-dropdown-item>
+                <b-dropdown id="userdropdown" v-bind:text="$store.state.user.username" v-if="$store.state.user.authenticated">
+                  <b-dropdown-item href="#">Favourites</b-dropdown-item>
+                  <b-dropdown-item href="#">Upload History</b-dropdown-item>
+                  <b-dropdown-item href="#">Settings</b-dropdown-item>
                 </b-dropdown>
 
                 <b-navbar-nav v-else>
@@ -16,7 +17,7 @@
 
                   <!-- Keep this centered -->
                   <b-navbar-nav class="mx-auto">
-                    <b-navbar-brand><router-link to="/">{{ brandName }}</router-link></b-navbar-brand>
+                    <b-navbar-brand><router-link class="navbar-brand" to="/">noot.<span class="brand-right">tech</span></router-link></b-navbar-brand>
                   </b-navbar-nav>
 
                   <b-navbar-nav class="ml-auto" v-if="$store.state.user.authenticated">
@@ -52,6 +53,20 @@
 <style scoped>
   .navbar.bg-dark {
     background-color: #202020 !important;
+    border-bottom: 1px solid #00cccc;
+  }
+
+  .navbar.bg-dark .navbar-brand {
+    position: absolute;
+    left: 50%;
+    top: 2px;
+    transform: translateX(-50%);
+    font-size: 24px;
+    border-bottom: 1px solid black;
+  }
+
+  .brand-right {
+    color: #00cccc;
   }
   .navbar a {
     color: #8f8f8f;
@@ -59,5 +74,11 @@
   .navbar a:hover {
     color: #d1d1d1;
     text-decoration: none;
+  }
+
+  .btn .btn-secondary .dropdown-toggle {
+    background-color: transparent !important;
+    border: none;
+    color: #969696;
   }
 </style>
