f69fb7cac4a4af74c5f219134f17c995cdbd2df9
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2019-02-23 06:24:14 +0000

    Nothing to see here, move along
    Ok seriously, I used whatthecommit to generate this commit.
    ** A programmer has been arrested: He refused to comment **
    
    And yes, I made the download button work. Move along!

--- NootTech/frontend/src/components/Utils/FilePreview/DownloadFile.vue
+++ NootTech/frontend/src/components/Utils/FilePreview/DownloadFile.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="download-container">
-    <b-button class="download-btn">Download File ({{ file.file_size_str }})</b-button>
+    <b-button class="download-btn" :href="file_position">Download File ({{ file.file_size_str }})</b-button>
+
   </div>
 </template>
 
 <script>
-    import BButton from "bootstrap-vue/src/components/button/button";
+  import {BACKEND_URL} from '../../../config.js';
+  import BButton from "bootstrap-vue/src/components/button/button";
 
-    export default {
-      name: "DownloadFile",
-      props: ['file'],
-      components: {BButton}
+  export default {
+    name: "DownloadFile",
+    props: ['file'],
+    components: {BButton},
+    computed: {
+      // TODO: Possible LFI attacks?
+      file_position() {
+        return BACKEND_URL + this.file.file_content;
+      }
     }
+  }
 </script>
 
 <style scoped>
