35bf269ec54db0964660bbaae483552e8d4afc3e
denBot <den.xyz@pm.me>
2019-03-20 22:18:40 +0000

    added static root to settings.py, configured template to load build.js if debug is off

--- NootTech/NootTech/settings.py
+++ NootTech/NootTech/settings.py
@@ -82,10 +82,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_REGEX_WHITELIST = (
-    r'^https?://127.0.0.1:8080$',
-    r'^https?://127.0.0.1:8000$',
-    r'^https?://'+DOMAIN_NAME+'$',
-    r'^https?://'+DOMAIN_NAME.replace(':8000',':8080')+'$',
+    r'^https?://'+DOMAIN_NAME.replace(".","\.")+'$',
+    r'^https?://'+DOMAIN_NAME.replace(".","\.").replace(':8000',':8080')+'$',
 )
 
 ADMIN_REORDER = (
@@ -276,8 +274,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'frontend/dist'),
 )
 
--- NootTech/NootTech/views.py
+++ NootTech/NootTech/views.py
@@ -1,8 +1,31 @@
 from django.views.generic import View
 from django.shortcuts import render
-
+from django.conf import settings
 
 class IndexView(View):
 
     def get(self, request, *args):
-        return render(request, 'index.html')
+
+        context = dict()
+        context["debug"] = settings.DEBUG
+        context["subdomain"] = False
+
+        subdomain = request.META['HTTP_HOST'].replace((settings.DOMAIN_NAME), "")[:-1]
+        
+        sharelink = '/u/' in request.build_absolute_uri()
+
+        url = request.build_absolute_uri()
+
+        if subdomain:
+            username = url.split("//")[0].split(".")[0]
+            genid = url.split("/")[-1]
+            u = User.objects.get(username=username)
+            f = File.objects.get(generated_filename=genid, user=u)
+        
+        elif sharelink:
+            username = url.split("/u/")[1].split("/")[0]
+            genid = url.split("/")[-1]
+            u = User.objects.get(username=username)
+            f = File.objects.get(generated_filename=genid, user=u)
+
+        return render(request, 'index.html', context=context)
