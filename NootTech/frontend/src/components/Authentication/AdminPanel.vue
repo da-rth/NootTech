c2ac00c11de2a073bc4cd77654ce7ce8fed80499
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2019-03-20 16:33:43 +0000

    almost fully implemented banning and deletion

--- NootTech/frontend/src/components/Authentication/AdminPanel.vue
+++ NootTech/frontend/src/components/Authentication/AdminPanel.vue
@@ -1,5 +1,29 @@
 <template>
  <div v-if="$store.state.settings && $store.state.settings.is_superuser" class="container">
+   <b-modal
+    v-if="selectedReport"
+    header-bg-variant="dark"
+    body-bg-variant="dark"
+    footer-bg-variant="dark"
+    header-border-variant="dark"
+    footer-border-variant="dark"
+    ref="banModal"
+    :title="`Ban the user ${selectedReport.reported_file.user.username}`"
+    ok-title="Ban"
+    ok-variant="danger"
+    @ok="banUser"
+    @hidden="resetBanPayload"
+    >
+    <b-form>
+      <b-form-group
+        label="Reason to ban the user"
+        label-for="banReason"
+        description="Please provide a reason for the ban">
+        <b-form-textarea id="banReason" v-model="banPayload.reason" rows="3" max-rows="10"/>
+      </b-form-group>
+
+    </b-form>
+   </b-modal>
    <h2>List of reported files</h2>
     <table class="table bg-dark striped">
       <thead>
@@ -25,8 +49,8 @@
           </td>
           <td scope="col">
             <b-button variant="warning" @click="warnUser(report)">Warn user</b-button>
-            <b-button variant="danger">Ban user</b-button>
-            <b-button variant="danger">Delete resource</b-button>
+            <b-button variant="danger" @click="banUserButton(report)">Ban user</b-button>
+            <b-button variant="danger" @click="deleteFile(report)">Delete resource</b-button>
           </td>
         </tr>
       </tbody>
@@ -42,7 +66,12 @@ export default {
   data() {
     return {
       reports: [],
+      selectedReport: null,
       config: config,
+      banPayload: {
+        user: 0,
+        reason: "",
+      }
     }
   },
   components: {NtBadge},
@@ -59,16 +88,49 @@ export default {
      */
     async warnUser(report) {
       console.log("Warning the user " + report.reported_file.user.username)
-      let response = await this.$api.WarnUser(
+      try {
+        let response = await this.$api.WarnUser(
                       {reason: `${report.reason_title}\n${report.reason_body}`,
                        warned_user: `${report.reported_file.user.id}`})
+      } catch(e) {
+        console.log("USER WARNING FAILED: ", e.response);
+      }
+    },
+    resetBanPayload() {
+      this.banPayload = {
+        user: 0,
+        reason: "",
+      }
+    },
+    banUserButton(report) {
+      this.selectedReport = report;
+      this.banPayload.user = report.reported_file.user.id;
+      this.$nextTick(() => {
+        this.$refs.banModal.show();
+      })
+    },
+    async banUser() {
+      try {
+        await this.$api.BanUser(this.banPayload);
+      } catch(e) {
+        console.log("USER BAN FAILED: ", e.response);
+      }
+    },
+
+    async deleteFile(report) {
+      try {
+        console.log("Deleting the file " + report.reported_file.id)
+      } catch(e) {
+        console.log("FILE DELETION FAILED: ", e.response)
+      }
     }
+    
  },
   async mounted() {
     try {
       let response = await this.$api.GetReports();
+      this.resetBanPayload();
       this.reports = response.data;
-      console.log(this.reports);
     } catch(e) {
       console.log(e.response);
     }
