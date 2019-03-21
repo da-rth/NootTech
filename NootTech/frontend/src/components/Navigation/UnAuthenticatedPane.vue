992debfd81c01a4360545d6584d7907638e735e4
denBot <den.xyz@pm.me>
2019-03-21 18:41:45 +0000

    fixed footer and also updated homepage, added extra howto

--- NootTech/frontend/src/components/Navigation/UnAuthenticatedPane.vue
+++ NootTech/frontend/src/components/Navigation/UnAuthenticatedPane.vue
@@ -2,15 +2,21 @@
    <!--<nt-main-pane title="NootTech: More than meets the m-eye-nd" >-->
     <div class="container-fluid">
       <div class="row">
-        <b-jumbotron header="Discover NootTech!"
-          class="bg-dark welcome-pane"
+        <b-jumbotron header="Welcome to NootTech!"
+          class="welcome-pane"
           :container-fluid="true"
           :fluid="true"
-          header-level=2
-          lead="Uploading has never been easier">
+          header-level=4
+          >
+          <br/>
+          <h5>This is a place where anyone can store their files, both privately and publicly. Access and share your content easily from anywhere!</h5>
+          <br/>
+          <h5>Uploading has never been easier!</h5>
+          <br/>
           <b-carousel
             id="mainCarousel"
             style="text-shadow: 1px 1px 2px #333;"
+            class="homepage-carousel"
             aria-controls="indicators"
             background="#ABABAB"
             :interval="5000"
@@ -19,34 +25,35 @@
             @sliding-start="onSlideStart"
             @sliding-end="onSlideEnd"
           >
-            <b-carousel-slide
-              img-src="https://cdn.discordapp.com/attachments/543489677412401153/558303824096395264/unknown2.png"
-            />
-            <b-carousel-slide
-              img-src="https://cdn.discordapp.com/attachments/543489677412401153/558304618237526027/unknown2.png"
-            />
-            <b-carousel-slide
-              img-src="https://cdn.discordapp.com/attachments/543489677412401153/558305155569811458/unknown2.png"
-            />
-            <b-carousel-slide
-              img-src="https://cdn.discordapp.com/attachments/543489677412401153/558306038449831942/unknown2.png"
-            />
+            <b-carousel-slide :img-src="$backend_url+'/media/slider1.png'"/>
+            <b-carousel-slide :img-src="$backend_url+'/media/slider2.png'"/>
+            <b-carousel-slide :img-src="$backend_url+'/media/slider3.png'"/>
+            <b-carousel-slide :img-src="$backend_url+'/media/slider4.png'"/>
           </b-carousel>
-          <br>
+          <br><br>
+          <h1>Have questions? Contact us!</h1>
+          <br/>
           <div class="row">
-            <a href="https://discord.gg/xKquqD9"
-              class="btn m-sm-auto m-md-auto m-lg-auto btn-lg">
+            <b-col-md-1></b-col-md-1>
+
+             <b-col-md-3>
+            <a href="https://discord.gg/xKquqD9" class="large-icon">
                 <font-awesome-icon :icon="['fab', 'discord']"/>
             </a>
-            <a href="mailto:contact@noot.tech"
-              class="btn m-sm-auto m-md-auto m-lg-auto btn-lg">
+             </b-col-md-3>
+
+             <b-col-md-3>
+            <a href="mailto:contact@noot.tech" class="large-icon">
                <font-awesome-icon :icon="['fas', 'envelope']"/>
             </a>
-            <a href="https://github.com/denBot/WAD2-Group-Project"
-              class="btn m-sm-auto m-md-auto m-lg-auto btn-lg">
+             </b-col-md-3>
+
+            <b-col-md-3>
+            <a href="https://github.com/denBot/WAD2-Group-Project" class="large-icon">
               <font-awesome-icon :icon="['fab', 'github']"/>
             </a>
-
+            </b-col-md-3>
+            <b-col-md-1></b-col-md-1>
           </div>
         </b-jumbotron>
       </div>
@@ -86,12 +93,46 @@
   .welcome-pane {
     text-align: center;
     width: 90vw;
-    background-color: #00000017 !important;
+    background-color: #202020 !important;
     border: 1px solid rgba(0,0,0,0.2);
+    margin: 30px;
     justify-content: center;
 }
 
 #mainCarousel {
   margin: 0 auto;
+}
+.large-icon {
+  font-size: 100px;
+  padding: 50px;
+}
+
+.large-icon svg {
+  -webkit-transition: -webkit-transform .5s ease-in-out;
+  -ms-transition: -ms-transform .5s ease-in-out;
+  transition: transform .5s ease-in-out; 
+}
+
+.large-icon:hover svg {
+  transform:rotate(360deg);
+  -ms-transform:rotate(360deg);
+  -webkit-transform:rotate(360deg);
+  color: #fff;
+}
+
+.large-icon svg path {
+  -webkit-transition: color .5s ease-in-out;
+  -ms-transition: color .5s ease-in-out;
+  transition: color .5s ease-in-out; 
+}
+
+.large-icon:hover svg path {
+  color: white;
+}
+
+.homepage-carousel {
+  width: 70%;
+  max-width: 90%;
+  border: 1px solid black;
 }
 </style>
---
+++ NootTech/media/Uploads/10/AkDiBmo22_myfile.py
@@ -0,0 +1,6 @@
+# This is a comment
+
+def wew():
+    print("this is a python file")
+
+wew()
